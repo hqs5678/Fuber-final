971c08342321acb308053c600639df88e881e4b6
HuangQingSong <hqs_5678@126.com>
2016-08-18 11:22:57 +0800

    为CAMediaTimingFunction 添加注解

--- SplashScreenUI/AnimatedULogoView.swift
+++ SplashScreenUI/AnimatedULogoView.swift
@@ -27,7 +27,11 @@ public class AnimatedULogoView: UIView {
     
     
     // CAMediaTimingFunction
-    
+    // 作用: 为动画设置加速度 减速度
+    // CAMediaTimingFunction(controlPoints c1x: Float, _ c1y: Float, _ c2x: Float, _ c2y: Float)
+    // 参数中有四个参数, 两个控制点, 设置控制点的位置, 来调整加速度
+    // 具体效果请参考 YouXianMing/Tween-o-Matic-CN  
+    // git:  https://github.com/YouXianMing/Tween-o-Matic-CN?spm=5176.100239.blogcont29568.8.Ds7q23
     
     private let strokeEndTimingFunction   = CAMediaTimingFunction(controlPoints: 1.00, 0.0, 0.35, 1.0)
     private let squareLayerTimingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0.0, 0.20, 1.0)
